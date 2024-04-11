ad42add7408d992447f9d37ae7a94cecdd163d11
GrewalBalraj <balraj.grewal.careers@gmail.com>
2024-04-11 16:44:18 -0400

    added dropdown box

--- bball/src/main/java/com/example/SchedulePane.java
+++ bball/src/main/java/com/example/SchedulePane.java
@@ -4,10 +4,8 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -25,6 +23,26 @@ public class SchedulePane {
         primaryStage.setTitle("Schedule Your Classes");
 
         StackPane stackPane = new StackPane();
+
+        ComboBox<String> comboBox = new ComboBox<>();
+        comboBox.getItems().addAll("Option 1", "Option 2", "Option 3", "Option 4", "Option 5");
+        comboBox.setPrefWidth(150);
+        comboBox.setVisibleRowCount(5);
+
+        Button yesButton = new Button("Yes");
+        yesButton.setDisable(true); 
+
+        comboBox.setOnAction(event -> {
+            if (comboBox.getValue() != null) {
+                yesButton.setDisable(false); 
+            }
+        });
+
+        VBox vBox = new VBox(10, comboBox, yesButton);
+        vBox.setAlignment(Pos.TOP_CENTER);
+        Insets margin = new Insets(50, 0, 0, 0);     
+        vBox.setPadding(margin);
+        stackPane.getChildren().add(vBox);
 
         Text title = new Text("Schedule Your Classes");
         stackPane.setAlignment(Pos.TOP_CENTER);
